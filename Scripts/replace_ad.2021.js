386df51bbf82746d0de90a2ce495ae06fad27f79
stop-robot <97998160+stop-robot@users.noreply.github.com>
2022-04-26 13:58:31 +0800

    Add files via upload

---
+++ Scripts/replace_ad.2021.js
@@ -0,0 +1,47 @@
+$(function(){
+	// #real_ad 580 x 400 
+	$('#real_ad').html('<script async src=\'https://securepubads.g.doubleclick.net/tag/js/gpt.js\'></script> \
+	<script> \
+	  window.googletag = window.googletag || {cmd: []}; \
+	  googletag.cmd.push(function() { \
+		googletag.defineSlot(\'/33575001/webmail580\', [580, 400], \'div-gpt-ad-1626220160583-0\').addService(googletag.pubads()); \
+		googletag.pubads().enableSingleRequest(); \
+		googletag.enableServices(); \
+	  }); \
+	</script><!-- /33575001/webmail580 --> \
+	<div id=\'div-gpt-ad-1626220160583-0\' style=\'min-width: 580px; min-height: 400px;\'> \
+	  <script> \
+		googletag.cmd.push(function() { googletag.display(\'div-gpt-ad-1626220160583-0\'); }); \
+	  </script> \
+	</div>');
+	// #real_ad2 970 x 90
+	$('#real_ad2').html('<script async src=\'https://securepubads.g.doubleclick.net/tag/js/gpt.js\'></script> \
+	<script> \
+	  window.googletag = window.googletag || {cmd: []}; \
+	  googletag.cmd.push(function() { \
+		googletag.defineSlot(\'/33575001/webmail970\', [970, 90], \'div-gpt-ad-1627289595083-0\').addService(googletag.pubads()); \
+		googletag.pubads().enableSingleRequest(); \
+		googletag.enableServices(); \
+	  }); \
+	</script> <!-- /33575001/webmail970 --> \
+	<div id=\'div-gpt-ad-1627289595083-0\'> \
+	  <script> \
+		googletag.cmd.push(function() { googletag.display(\'div-gpt-ad-1627289595083-0\'); }); \
+	  </script> \
+	</div>');
+	// #real_ad3 300 x 250
+	$('#real_ad3').html('<script async src=\'https://securepubads.g.doubleclick.net/tag/js/gpt.js\'></script> \
+	<script> \
+	  window.googletag = window.googletag || {cmd: []}; \
+	  googletag.cmd.push(function() { \
+		googletag.defineSlot(\'/33575001/mail300250in\', [300, 250], \'div-gpt-ad-1626222356382-0\').addService(googletag.pubads()); \
+		googletag.pubads().enableSingleRequest(); \
+		googletag.enableServices(); \
+	  }); \
+	</script><!-- /33575001/mail300250in --> \
+	<div id=\'div-gpt-ad-1626222356382-0\' style=\'min-width: 300px; min-height: 250px;\'> \
+	  <script> \
+		googletag.cmd.push(function() { googletag.display(\'div-gpt-ad-1626222356382-0\'); }); \
+	  </script> \
+	</div>');
+});
